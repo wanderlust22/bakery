9cce05f6cf19df90e33488f25a43a51f8c643a03
Will Quanstrom <wquanstr215@gmail.com>
2019-12-13 12:52:58 -0800

    add front-end dialogue

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ class Program
 {
     static void Main()
     {
-        
+        Console.WriteLine("Bon jour! I am Pierre, welcome to my bakery monsieur and madame.");
+        Thread.Sleep(1000);
+        Console.WriteLine("We have a lovely selection of bread and pastries to choose from.");
     }
 }
