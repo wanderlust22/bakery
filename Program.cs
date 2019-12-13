d8db524940b485edfce36f4b74e84f9ff59b6488
Will Quanstrom <wquanstr215@gmail.com>
2019-12-13 11:25:00 -0800

    add functionality to classes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collenctions.Generic;
+using System.Threading;
+using Bakery;
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
--- bakery.cs
+++ bakery.cs
@@ -4,9 +4,16 @@ namespace Bakery
 {
     class Bread
     {
-
+        private int _loaves;
+        private int _price;
     }
     class Pastry
+    {
+        private int _pastries;
+        private int _price;
+    }
+
+    public int Price()
     {
         
     }
