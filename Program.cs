cbb2b91354dd0fd6ba28ce8092f1d41cfae56cc6
Will Quanstrom <wquanstr215@gmail.com>
2019-12-13 15:40:10 -0800

    finish all functionality

--- Program.cs
+++ Program.cs
@@ -7,16 +7,18 @@ class Program
 {
     static void Main()
     {
+            
         Console.WriteLine("Bon jour! I am Pierre, welcome to my bakery monsieur and madame.");
         Thread.Sleep(1000);
         Console.WriteLine("We have a lovely selection of bread and pastries to choose from.");
-        Thread.Sleep(1000);
+        Thread.Sleep(1500);
         Console.WriteLine("What will you be needing today? A loaf of bread is $5. And a Pastry is $2.");
-        Thread.Sleep(1000);
+        Thread.Sleep(2000);
         Console.WriteLine("The deals are as follows:");
+        Thread.Sleep(1500);
         Console.WriteLine("If you buy two loaves of bread, you get the third one free!");
         Console.WriteLine("And it's buy 3 pastries for 5$!");
-        Thread.Sleep(1000);
+        Thread.Sleep(2000);
         Console.WriteLine("What can Pierre get for you today? How many loaves do you need?");
         string stringLoaves = Console.ReadLine();
         int loaves = int.Parse(stringLoaves);
@@ -25,9 +27,10 @@ class Program
         int pastries = int.Parse(stringPastries);
 
         Bread breadOrder = new Bread(loaves, 5);
-        // Pastry pastryOrder = new Pastry();
-        Console.WriteLine(breadOrder._price);
-
-
+        Pastry pastryOrder = new Pastry(pastries, 2);
+       
+        int total = breadOrder.GetBreadPrice() + pastryOrder.GetPastryPrice();
+        Console.WriteLine("You are Pierre's favorite customer! You owe me $" + breadOrder.GetBreadPrice() + " for your loaves of bread, and $" + pastryOrder.GetPastryPrice() + " for your pastries. Your total comes to $" + total + " See you soon!" );
+        
     }
 }
--- bakery.cs
+++ bakery.cs
@@ -13,6 +13,29 @@ namespace Bakery
             _price = price;
         }
         
+        public int GetBreadPrice()
+        {
+            if (_loaves == 2)
+            {
+                Console.WriteLine("Pierre will give you a third at no extra charge!");
+                return _loaves * _price;
+            }
+            
+            else if (_loaves / 3 >= 1)
+            {
+                double divide = _loaves / 3;
+                double divideFloor = Math.Floor(divide);
+                int subtract = Convert.ToInt32(divideFloor);
+
+                int toPayFor = _loaves - subtract;
+                return toPayFor * _price;
+            }
+            else
+            {
+                return _loaves * _price;
+            }
+
+        }
     }
     class Pastry
     {
@@ -24,11 +47,21 @@ namespace Bakery
             _pastries = pastries;
             _price = price;
         }
+
+        public int GetPastryPrice()
+        {
+            if (_pastries / 3 >= 1)
+            {
+                double divide = _pastries / 3;
+                double divideFloor = Math.Floor(divide);
+                int subtract = Convert.ToInt32(divideFloor);
+
+                return (_pastries * _price) - subtract;
+            }
+            else
+            {
+                return _pastries * _price;
+            }
+        }  
     }
-
-    // public int GetPrice()
-
-
-
-
 }
